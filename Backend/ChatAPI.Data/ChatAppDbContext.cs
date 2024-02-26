b79c626e670f909ac62265f61ed34310e436fa1e
j3luelarck <cosioslx@hotmail.com>
2024-02-26 21:16:45 +0200

    #3 Domain Class Implementation
    Implemented DbContext properties and validations with fluentAPI

--- Backend/ChatAPI.Data/ChatAppDbContext.cs
+++ Backend/ChatAPI.Data/ChatAppDbContext.cs
@@ -1,3 +1,4 @@
+using ChatAPI.Data.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace ChatAPI.Data;
@@ -17,6 +18,44 @@ public class ChatAppDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Message>()
+            .Property(x => x.Text)
+            .IsRequired()
+            .HasMaxLength(500);
+        modelBuilder.Entity<Message>()
+            .Property(x => x.SenderId)
+            .IsRequired();
+        modelBuilder.Entity<Message>()
+            .Property(x => x.ChatRoomId)
+            .IsRequired();
+        modelBuilder.Entity<Message>()
+            .HasMany<ChatRoom>(x => x.ChatRooms)
+            .WithMany(x => x.Messages);
+
+        modelBuilder.Entity<ChatRoomUser>()
+            .Property(x => x.ChatRoomId)
+            .IsRequired();
+        modelBuilder.Entity<ChatRoomUser>()
+            .Property(x => x.UserId)
+            .IsRequired();
+        modelBuilder.Entity<ChatRoomUser>()
+            .Property(x => x.Description)
+            .IsRequired();
+        modelBuilder.Entity<ChatRoomUser>()
+            .HasMany<ChatRoom>(x => x.ChatRooms)
+            .WithMany(x => x.ChatRoomUsers);
+
+        modelBuilder.Entity<ChatRoom>()
+            .Property(x => x.Name)
+            .IsRequired();
+        modelBuilder.Entity<ChatRoom>()
+            .Property(x => x.IsPrivateChat);
+        modelBuilder.Entity<ChatRoom>()
+            .Property(x => x.IsOneToOneChat);
+        modelBuilder.Entity<ChatRoom>()
+            .HasMany<ChatRoomUser>(x => x.ChatRoomUsers)
+            .WithMany(x => x.ChatRooms);
+            
         base.OnModelCreating(modelBuilder);
     }
 }
